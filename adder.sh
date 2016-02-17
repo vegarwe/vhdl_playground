27739fd14ebbf6933a07a61332efb20155ab1a68
Vegar Westerlund <vegarwe@gmail.com>
2016-02-17 19:35:22 +0100

    Cleanup

--- adder.sh
+++ adder.sh
@@ -3,34 +3,14 @@
 set -e
 
 function clean {
-    mkdir -p work
+    mkdir -p work osvvm
     ghdl --clean --workdir=work
+    ghdl --clean --workdir=osvvm
     #ghdl --remove --workdir=work
     #rm -rf work
 }
 
-function compile {
-    mkdir -p work
-    #ghdl -a --workdir=work --std=08 --ieee=standard adder.vhdl
-    #ghdl -a --workdir=work --std=08 --ieee=standard adder_tb.vhdl
-    #ghdl -e --workdir=work --std=08 --ieee=standard adder_tb
-    ghdl -c --workdir=work --std=08 --ieee=standard -Posvvm *.vhdl -e adder_tb
-}
-
-function run {
-    ghdl -r adder_tb
-}
-
-function wave {
-    mkdir -p work
-    ghdl -r adder_tb --vcd=work/adder.vcd
-    gtkwave work/adder.vcd 2> /dev/null
-}
-
 function build_OSVVM {
-    #ghdl -a --std=08 --work=ieee_proposed ../OS-VVM_2_3_1/vhdl_2002_versions/standard_additions_c.vhdl
-    #ghdl -a --std=08 --work=ieee_proposed ../OS-VVM_2_3_1/vhdl_2002_versions/standard_textio_additions_c.vhdl
-
     mkdir -p osvvm
     ghdl -a --std=08 --work=osvvm --workdir=osvvm --ieee=standard OSVVM/NamePkg.vhd
     ghdl -a --std=08 --work=osvvm --workdir=osvvm --ieee=standard OSVVM/OsvvmGlobalPkg.vhd
@@ -51,9 +31,24 @@ function build_OSVVM {
     #ghdl -a --std=08 --work=OSVVM --workdir=OSVVM --ieee=standard OSVVM/demo/Demo_Rand.vhd
     #hdl -e --std=08 --work=OSVVM --workdir=OSVVM --ieee=standard Demo_Rand
     #./Demo_rand
+}
 
+function compile {
+    mkdir -p work
+    #ghdl -a --workdir=work --std=08 --ieee=standard adder.vhdl
+    #ghdl -a --workdir=work --std=08 --ieee=standard adder_tb.vhdl
+    #ghdl -e --workdir=work --std=08 --ieee=standard adder_tb
+    ghdl -c --workdir=work --std=08 --ieee=standard -Posvvm *.vhdl -e adder_tb
+}
 
+function run {
+    ghdl -r adder_tb
+}
 
+function wave {
+    mkdir -p work
+    ghdl -r adder_tb --vcd=work/adder.vcd
+    gtkwave work/adder.vcd 2> /dev/null
 }
 
 clean
